0848414cbeb0867c2fa5ff93c3397afff76c63b6
Amity122 <amityelmwood@gmail.com>
2024-09-10 13:25:43 +0800

    Added baselayout and styling

---
+++ src/components/base/BaseLayout.vue
@@ -0,0 +1,38 @@
+<template>
+    <ion-menu content-id="main-content">
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>Something, something something</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-padding">This is the menu content.</ion-content>
+    </ion-menu>
+    <ion-page id="main-content">
+        <ion-header>
+            <ion-toolbar>
+                <ion-buttons slot="start">
+                    <ion-menu-button></ion-menu-button>
+                </ion-buttons>
+                <ion-title>{{ pageTitle }}</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-padding"> Tap the button in the toolbar to open the menu. </ion-content>
+    </ion-page>
+</template>
+<script>
+import { IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+
+export default {
+    props: ['pageTitle'],
+    components: {
+      IonButtons,
+      IonContent,
+      IonHeader,
+      IonMenu,
+      IonMenuButton,
+      IonPage,
+      IonTitle,
+      IonToolbar,
+ }
+}
+</script>
